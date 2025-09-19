586b7945b591d73c80b26a54c40d3be862cc59a6
andrii-vitiuk <imperial_andr@ukr.net>
2025-09-19 18:36:31 +0300

    HW1 impl

---
+++ task1/factory.py
@@ -0,0 +1,89 @@
+import logging
+from abc import ABC, abstractmethod
+
+# logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+# 1. Abstract Vehicle
+class Vehicle(ABC):
+    def __init__(self, make: str, model: str):
+        self.make = make
+        self.model = model
+
+    @abstractmethod
+    def start_engine(self) -> None:
+        pass
+
+
+# 2.  Car and Motorcycle from Vehicle
+class Car(Vehicle):
+    def __init__(self, make: str, model: str, spec: str):
+        super().__init__(make, model)
+        self.spec = spec
+
+    def start_engine(self) -> None:
+        logger.info("%s %s (%s): Двигун запущено", self.make, self.model, self.spec)
+
+
+class Motorcycle(Vehicle):
+    def __init__(self, make: str, model: str, spec: str):
+        super().__init__(make, model)
+        self.spec = spec
+
+    def start_engine(self) -> None:
+        logger.info("%s %s (%s): Мотор заведено", self.make, self.model, self.spec)
+
+
+# 3. Abstract VehicleFactory
+class VehicleFactory(ABC):
+    @abstractmethod
+    def create_car(self, make: str, model: str) -> Car:
+        pass
+
+    @abstractmethod
+    def create_motorcycle(self, make: str, model: str) -> Motorcycle:
+        pass
+
+
+# 4. Fabric impl
+class USVehicleFactory(VehicleFactory):
+    def create_car(self, make: str, model: str) -> Car:
+        return Car(make, model, "US Spec")
+
+    def create_motorcycle(self, make: str, model: str) -> Motorcycle:
+        return Motorcycle(make, model, "US Spec")
+
+
+class EUVehicleFactory(VehicleFactory):
+    def create_car(self, make: str, model: str) -> Car:
+        return Car(make, model, "EU Spec")
+
+    def create_motorcycle(self, make: str, model: str) -> Motorcycle:
+        return Motorcycle(make, model, "EU Spec")
+
+
+# 5. run
+def main():
+    # Створення
+    us_factory = USVehicleFactory()
+    eu_factory = EUVehicleFactory()
+
+    # US фабрики
+    vehicle1 = us_factory.create_car("Ford", "Mustang")
+    vehicle1.start_engine()
+
+    vehicle2 = us_factory.create_motorcycle("Harley-Davidson", "Sportster")
+    vehicle2.start_engine()
+
+    # EU фабрики
+    vehicle3 = eu_factory.create_car("BMW", "3 Series")
+    vehicle3.start_engine()
+
+    vehicle4 = eu_factory.create_motorcycle("Ducati", "Monster")
+    vehicle4.start_engine()
+
+
+if __name__ == "__main__":
+    main()
---
+++ task2/solid.py
@@ -0,0 +1,113 @@
+import logging
+from abc import ABC, abstractmethod
+from typing import List
+
+# logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+# 1. SRP - Book 
+class Book:
+    def __init__(self, title: str, author: str, year: str) -> None:
+        self.title = title
+        self.author = author
+        self.year = year
+    
+    def __str__(self) -> str:
+        return "Title: %s, Author: %s, Year: %s" % (self.title, self.author, self.year)
+    
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Book):
+            return False
+        return self.title == other.title
+
+
+# 4. ISP - Interface lib
+class LibraryInterface(ABC):
+    @abstractmethod
+    def add_book(self, book: Book) -> None:
+        pass
+    
+    @abstractmethod
+    def remove_book(self, title: str) -> bool:
+        pass
+    
+    @abstractmethod
+    def get_books(self) -> List[Book]:
+        pass
+
+
+# 3. LSP - can be replaced by any LibraryInterface
+class Library(LibraryInterface):
+    def __init__(self) -> None:
+        self.books: List[Book] = []
+    
+    def add_book(self, book: Book) -> None:
+        if book not in self.books:
+            self.books.append(book)
+            logger.info("Book '%s' added", book.title)
+        else:
+            logger.info("Book '%s' already exists", book.title)
+    
+    def remove_book(self, title: str) -> bool:
+        for book in self.books:
+            if book.title == title:
+                self.books.remove(book)
+                logger.info("Book '%s' removed", title)
+                return True
+        logger.info("Book '%s' not found", title)
+        return False
+    
+    def get_books(self) -> List[Book]:
+        return self.books.copy()
+
+
+# 5. DIP - LibraryManager
+class LibraryManager:
+    def __init__(self, library: LibraryInterface) -> None:
+        self.library = library
+    
+    def add_book(self, title: str, author: str, year: str) -> None:
+        book = Book(title, author, year)
+        self.library.add_book(book)
+    
+    def remove_book(self, title: str) -> None:
+        self.library.remove_book(title)
+    
+    def show_books(self) -> None:
+        books = self.library.get_books()
+        if books:
+            logger.info("Books in library:")
+            for book in books:
+                logger.info(str(book))
+        else:
+            logger.info("Library is empty")
+
+
+def main():
+    library = Library()
+    manager = LibraryManager(library)
+
+    while True:
+        command = input("Enter command (add, remove, show, exit): ").strip().lower()
+
+        match command:
+            case "add":
+                title = input("Enter book title: ").strip()
+                author = input("Enter book author: ").strip()
+                year = input("Enter book year: ").strip()
+                manager.add_book(title, author, year)
+            case "remove":
+                title = input("Enter book title to remove: ").strip()
+                manager.remove_book(title)
+            case "show":
+                manager.show_books()
+            case "exit":
+                break
+            case _:
+                logger.info("Invalid command. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
